6fe28e21915048665e10adf71ae3a0eb9654bc81
ciaran brady <ciaranbrady559@gmail.com>
2017-11-16 11:34:17 +0000

    Second commit csgo+ tournament changes 2.0

--- ResultsApp/src/resultsapp/CSGO.java
+++ ResultsApp/src/resultsapp/CSGO.java
@@ -9,6 +9,39 @@ package resultsapp;
  *
  * @author dell
  */
-public class CSGO {
+public class CSGO extends Results{
+    String team;
+    String role;
+    String sponsor;
     
+
+    public CSGO(String team, String role, String sponsor) {
+        this.team = team;
+        this.role = role;
+        this.sponsor= sponsor;
+    }
+    
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    public String getSponsor() {
+        return sponsor;
+    }
+    
+    public String setSponsor() {
+        this.sponsor=sponsor;
+    }
 }
